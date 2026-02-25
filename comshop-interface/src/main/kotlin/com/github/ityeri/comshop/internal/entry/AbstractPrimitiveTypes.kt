ff820399f29d6fa2ec29112def06ba60ab7cf81e
it <ityeri9@gmail.com>
2026-02-26 04:02:13 +0900

    add: Add additional parameters to primitive types

--- comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/entry/AbstractPrimitiveTypes.kt
+++ comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/entry/AbstractPrimitiveTypes.kt
@@ -4,10 +4,10 @@ import com.github.ityeri.comshop.internal.argument.ComshopArgumentType
 
 interface AbstractPrimitiveTypes {
     fun bool(): ComshopArgumentType<Boolean>
-    fun int(): ComshopArgumentType<Int>
-    fun float(): ComshopArgumentType<Float>
-    fun double(): ComshopArgumentType<Double>
-    fun long(): ComshopArgumentType<Long>
+    fun int(min: Int = Int.MIN_VALUE, max: Int = Int.MAX_VALUE): ComshopArgumentType<Int>
+    fun float(min: Float = Float.MIN_VALUE, max: Float = Float.MAX_VALUE): ComshopArgumentType<Float>
+    fun double(min: Double = Double.MIN_VALUE, max: Double = Double.MAX_VALUE): ComshopArgumentType<Double>
+    fun long(min: Long = Long.MIN_VALUE, max: Long = Long.MAX_VALUE): ComshopArgumentType<Long>
     fun string(type: StringType = StringType.WORD): ComshopArgumentType<String>
 }
 
