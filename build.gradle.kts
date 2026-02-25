2f8e61dbb22ebda0dc6d8a0d73aad45c7d20a237
it <ityeri9@gmail.com>
2026-02-26 03:18:52 +0900

    refactor: Add multi module build setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
-plugins {
-    kotlin("jvm") version "2.2.0"
-}
+allprojects {
+    repositories {
+        mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/") {
+            name = "papermc"
+        }
+    }
 
-repositories {
-    mavenCentral()
-}
-
-subprojects {
     project.group = "com.github.ityeri.comshop"
     project.version = "v1.0.0-beta.3"
 }
