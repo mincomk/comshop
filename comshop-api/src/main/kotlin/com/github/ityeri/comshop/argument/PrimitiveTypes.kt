d8a6d454f39791368280b5d7c3870cc34f76a168
it <ityeri9@gmail.com>
2026-02-27 00:09:03 +0900

    edit: Change ptimitive types api fit to changed interface

--- comshop-api/src/main/kotlin/com/github/ityeri/comshop/argument/PrimitiveTypes.kt
+++ comshop-api/src/main/kotlin/com/github/ityeri/comshop/argument/PrimitiveTypes.kt
@@ -9,9 +9,9 @@ object PrimitiveTypes : AbstractPrimitiveTypes {
     private val types = ComshopBootstrapper.loader!!.loadPrimitiveTypes()
 
     override fun bool(): ComshopArgumentType<Boolean> = types.bool()
-    override fun int(): ComshopArgumentType<Int> = types.int()
-    override fun float(): ComshopArgumentType<Float> = types.float()
-    override fun double(): ComshopArgumentType<Double> = types.double()
-    override fun long(): ComshopArgumentType<Long> = types.long()
-    override fun string(type: StringType): ComshopArgumentType<String> = types.string()
+    override fun int(min: Int, max: Int): ComshopArgumentType<Int> = types.int(min, max)
+    override fun float(min: Float, max: Float): ComshopArgumentType<Float> = types.float(min, max)
+    override fun double(min: Double, max: Double): ComshopArgumentType<Double> = types.double(min, max)
+    override fun long(min: Long, max: Long): ComshopArgumentType<Long> = types.long(min, max)
+    override fun string(type: StringType): ComshopArgumentType<String> = types.string(type)
 }
