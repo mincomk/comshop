78e1f237a0707fc483e83e8ea7c9bcb6d8d335f2
it <ityeri9@gmail.com>
2026-03-04 08:20:01 +0900

    add: ArgumentNodeType impl

---
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/ArgumentNodeType.kt
@@ -0,0 +1,15 @@
+package com.github.ityeri.comshop.internal.impl
+
+import com.github.ityeri.comshop.internal.argument.ComshopArgumentType
+import com.github.ityeri.comshop.internal.node.type.AbstractArgumentNodeType
+import com.mojang.brigadier.builder.ArgumentBuilder
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
+
+class ArgumentNodeType<S, T>(
+    override val name: String,
+    override val argumentType: ComshopArgumentType<T>
+) : AbstractArgumentNodeType<S, T> {
+    override fun createBuilder(): ArgumentBuilder<S, *> {
+        return RequiredArgumentBuilder.argument<S, T>(name, argumentType.build())
+    }
+}
