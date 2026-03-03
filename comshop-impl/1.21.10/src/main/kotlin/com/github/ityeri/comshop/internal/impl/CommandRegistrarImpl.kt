6fd4a7ce0cb2896dea8058de56e03783b8a6fab9
it <ityeri9@gmail.com>
2026-03-04 07:32:01 +0900

    add: CommandRegistrar impl

--- comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandRegistrarImpl.kt
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandRegistrarImpl.kt
@@ -3,14 +3,24 @@ package com.github.ityeri.comshop.internal.impl
 import com.github.ityeri.comshop.internal.entry.AbstractCommandRegistrar
 import com.github.ityeri.comshop.internal.node.Node
 import com.github.ityeri.comshop.internal.node.type.NodeType
+import com.mojang.brigadier.tree.LiteralCommandNode
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.plugin.java.JavaPlugin
 
-class CommandRegistrarImpl<S> : AbstractCommandRegistrar<S> {
+class CommandRegistrarImpl : AbstractCommandRegistrar<CommandSourceStack> {
+    val nodes: MutableList<Node<NodeType<CommandSourceStack>>> = mutableListOf()
+
     override fun init(plugin: JavaPlugin) {
-        TODO("Not yet implemented")
+        plugin.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
+            for (node in nodes) {
+                val boundary = createBoundary(node)
+                commands.registrar().register(boundary.entries.first().build() as LiteralCommandNode)
+            }
+        }
     }
 
-    override fun register(node: Node<NodeType<S>>) {
-        TODO("Not yet implemented")
+    override fun register(node: Node<NodeType<CommandSourceStack>>) {
+        nodes.add(node)
     }
 }
