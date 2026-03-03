cecda6883ae5af3b2d077ec7c331e05547f328c6
it <ityeri9@gmail.com>
2026-03-03 19:32:43 +0900

    edit: Change api classes to class delegation

--- comshop-api/src/main/kotlin/com/github/ityeri/comshop/CommandRegistrar.kt
+++ comshop-api/src/main/kotlin/com/github/ityeri/comshop/CommandRegistrar.kt
@@ -1,20 +1,6 @@
 package com.github.ityeri.comshop
 
 import com.github.ityeri.comshop.internal.entry.AbstractCommandRegistrar
-import com.github.ityeri.comshop.internal.argument.ComshopArgumentType
-import com.github.ityeri.comshop.internal.node.Node
-import com.github.ityeri.comshop.internal.node.type.NodeType
 import io.papermc.paper.command.brigadier.CommandSourceStack
-import org.bukkit.plugin.java.JavaPlugin
 
-object CommandRegistrar : AbstractCommandRegistrar<CommandSourceStack> {
-    private val registrar = ComshopBootstrapper.loader!!.loadRegistrar()
-
-    override fun init(plugin: JavaPlugin) {
-        registrar.init(plugin)
-    }
-
-    override fun register(node: Node<NodeType<CommandSourceStack>>) {
-        registrar.register(node)
-    }
-}
+object CommandRegistrar : AbstractCommandRegistrar<CommandSourceStack> by ComshopBootstrapper.loader!!.loadRegistrar()
