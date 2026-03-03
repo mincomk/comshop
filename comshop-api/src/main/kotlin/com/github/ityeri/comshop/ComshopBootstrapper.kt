062a615b78b594097a97d00336fc47ac5cfcc371
it <ityeri9@gmail.com>
2026-03-03 19:47:30 +0900

    fix: Remove internal keyword from ComshopBootstrapper

--- comshop-api/src/main/kotlin/com/github/ityeri/comshop/ComshopBootstrapper.kt
+++ comshop-api/src/main/kotlin/com/github/ityeri/comshop/ComshopBootstrapper.kt
@@ -2,16 +2,23 @@ package com.github.ityeri.comshop
 
 import com.github.ityeri.comshop.internal.entry.AbstractComshopLoader
 
-internal object ComshopBootstrapper {
+object ComshopBootstrapper {
     var loaderClassPath: String = "com.github.ityeri.comshop.internal.impl.ComshopLoaderImpl"
-    var loader: AbstractComshopLoader? = null
+    internal var loader: AbstractComshopLoader? = null
+        get() = if (field != null) {
+            field
+        } else {
+            throw IllegalStateException("Loader is not loaded yet. Please call ComshopBootstrapper.load() first")
+        }
 
     fun load() {
         try {
             val clazz = Class.forName(loaderClassPath)
             loader = clazz.getDeclaredConstructor().newInstance() as AbstractComshopLoader
         } catch (e: ClassNotFoundException) {
-            throw ClassNotFoundException("loaderClassPath invalid or cannot find loader class")
+            throw ClassNotFoundException(
+                "loaderClassPath is invalid or cannot find loader class. Is comshop impl dependency added?"
+            )
         }
     }
 }
