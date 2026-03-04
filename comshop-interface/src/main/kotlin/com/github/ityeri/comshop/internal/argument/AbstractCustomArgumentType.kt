a49985911cb770b6372ae8d684b739e67885bac9
it <ityeri9@gmail.com>
2026-03-04 12:10:36 +0900

    add: Restore native type generic

--- comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/argument/AbstractCustomArgumentType.kt
+++ comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/argument/AbstractCustomArgumentType.kt
@@ -3,8 +3,8 @@ package com.github.ityeri.comshop.internal.argument
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.context.CommandContext
 
-interface AbstractCustomArgumentType<T> : ComshopArgumentType<T> {
-    val nativeArgumentType: NativeArgumentType<*>
+interface AbstractCustomArgumentType<T, N> : ComshopArgumentType<T> {
+    val nativeArgumentType: NativeArgumentType<N>
 
     fun parse(reader: StringReader): T
     fun <S> suggests(context: CommandContext<S>, remineText: String): Collection<String>
