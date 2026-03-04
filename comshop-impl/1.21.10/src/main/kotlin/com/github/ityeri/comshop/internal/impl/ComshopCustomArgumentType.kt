a5802ac2859c5f211713300ff2dd69bedf158258
it <ityeri9@gmail.com>
2026-03-04 12:17:22 +0900

    add: ComshopCustomArgumentType impl

---
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/ComshopCustomArgumentType.kt
@@ -0,0 +1,50 @@
+package com.github.ityeri.comshop.internal.impl
+
+import com.github.ityeri.comshop.internal.argument.AbstractCustomArgumentType
+import com.github.ityeri.comshop.internal.argument.NativeArgumentType
+import com.mojang.brigadier.StringReader
+import com.mojang.brigadier.arguments.ArgumentType
+import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.suggestion.Suggestions
+import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType
+import java.util.concurrent.CompletableFuture
+
+
+open class ComshopCustomArgumentType<T : Any, N : Any>(
+    override val nativeArgumentType: NativeArgumentType<N>
+) : AbstractCustomArgumentType<T, N> {
+    override fun parse(reader: StringReader): T {
+        throw NotImplementedError()
+    }
+
+    override fun <S> suggests(
+        context: CommandContext<S>,
+        remineText: String
+    ): Collection<String> {
+        throw NotImplementedError()
+    }
+
+    override fun build(): ArgumentType<T> {
+        return object : CustomArgumentType<T, N> {
+            override fun parse(reader: StringReader): T {
+                return this@ComshopCustomArgumentType.parse(reader)
+            }
+
+            override fun getNativeType(): ArgumentType<N> {
+                return nativeArgumentType.build()
+            }
+
+            override fun <S : Any> listSuggestions(
+                context: CommandContext<S>,
+                builder: SuggestionsBuilder
+            ): CompletableFuture<Suggestions> {
+                this@ComshopCustomArgumentType.suggests(context, builder.remaining).forEach { text ->
+                    builder.suggest(text)
+                }
+
+                return builder.buildFuture()
+            }
+        }
+    }
+}
