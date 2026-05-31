7c462995bb0935990e577e684898ef7ce5b1abcf
ityeri <ityeri9@gmail.com>
2026-05-31 14:23:39 +0900

    rm: Remove NodeType which is unnecessary by structure refactoring

--- comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/node/type/NodeType.kt
+++
@@ -1,7 +0,0 @@
-package com.github.ityeri.comshop.internal.node.type
-
-import com.mojang.brigadier.builder.ArgumentBuilder
-
-interface NodeType<S> {
-    fun createBuilder(): ArgumentBuilder<S, *>
-}
