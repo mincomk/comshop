9444f7fe52cf35539c636316aaeb218f5dc2ac82
ityeri <ityeri9@gmail.com>
2026-06-01 00:23:59 +0900

    add: Added CommandFragment for ExecuteNode converting

---
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandFragment.kt
@@ -0,0 +1,11 @@
+package com.github.ityeri.comshop.internal.impl
+
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
+import io.papermc.paper.command.brigadier.CommandSourceStack
+
+
+sealed class CommandFragment {
+    class ArgumentFragment<T>(val builder: RequiredArgumentBuilder<CommandSourceStack, T>) : CommandFragment()
+    class ExecuteFragment(val command: Command<CommandSourceStack>) : CommandFragment()
+}
