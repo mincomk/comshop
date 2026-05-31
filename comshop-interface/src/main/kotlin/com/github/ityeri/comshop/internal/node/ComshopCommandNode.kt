8c8b12d8d52fdcca2eb0805bf31cced73bad30b7
ityeri <ityeri9@gmail.com>
2026-05-31 20:50:41 +0900

    add: Restore deleted ComshopCommandNode.ExecuteNode

--- comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/node/ComshopCommandNode.kt
+++ comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/node/ComshopCommandNode.kt
@@ -8,12 +8,15 @@ sealed class ComshopCommandNode {
     class LiteralCommandNode(
         val name: String,
         val requiresChecker: (CommandSourceStack) -> Boolean = { true },
-        val commandBlock: (ComshopContext) -> Unit
     ) : ComshopCommandNode()
 
     class ArgumentNode<T>(
         val name: String,
         val argumentType: ComshopArgumentType<T>,
         val requiresChecker: (CommandSourceStack) -> Boolean = { true }
+    ) : ComshopCommandNode()
+
+    class ExecuteNode(
+        val commandBlock: (ComshopContext) -> Unit
     ) : ComshopCommandNode()
 }
