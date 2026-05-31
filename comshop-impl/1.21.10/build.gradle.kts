0d4a7c48f56a8472c2dd97f0a736b40229673fab
ityeri <ityeri9@gmail.com>
2026-05-31 14:29:46 +0900

    add: Added arrow-kt dependency for node converting & compiling

--- comshop-impl/1.21.10/build.gradle.kts
+++ comshop-impl/1.21.10/build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
 
 dependencies {
     api("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    implementation("io.arrow-kt:arrow-core:1.2.4")
+    implementation("io.arrow-kt:arrow-optics:1.2.4")
 }
