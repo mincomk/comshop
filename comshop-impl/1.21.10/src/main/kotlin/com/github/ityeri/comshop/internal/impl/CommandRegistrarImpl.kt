6a91150c0886f7c5196e18e4e88c688da10a717b
ityeri <ityeri9@gmail.com>
2026-05-31 17:01:29 +0900

    add: Add node converting & registration code

--- comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandRegistrarImpl.kt
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandRegistrarImpl.kt
@@ -3,15 +3,28 @@ package com.github.ityeri.comshop.internal.impl
 import com.github.ityeri.comshop.internal.entry.AbstractCommandRegistrar
 import com.github.ityeri.comshop.internal.node.Node
 import com.github.ityeri.comshop.internal.node.type.ComshopCommandNode
+import com.mojang.brigadier.tree.LiteralCommandNode
+import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.plugin.java.JavaPlugin
 
+
 class CommandRegistrarImpl : AbstractCommandRegistrar {
     val nodes: MutableList<Node<ComshopCommandNode>> = mutableListOf()
+
     override fun init(plugin: JavaPlugin) {
-        plugin.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
+        plugin.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { event ->
+            val commands = event.registrar()
+
             for (node in nodes) {
-//                val boundary = createBoundary(node)
+                val convertedNode = convertToArgumentBuilderNode(node)
+                val boundary = connectArgumentBuilders(convertedNode)
+
+                if (boundary.entries.size != 1) {
+                    throw IllegalArgumentException("??")
+                }
+
+                commands.register(boundary.entries.first().build() as LiteralCommandNode<CommandSourceStack>)
             }
         }
     }
