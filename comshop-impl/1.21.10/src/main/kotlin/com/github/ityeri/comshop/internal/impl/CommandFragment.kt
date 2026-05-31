c7d11a281ceeb9a059ac98d531dcf545f764cc69
ityeri <ityeri9@gmail.com>
2026-06-01 08:37:41 +0900

    edit: Changed CommandFragment.builder field type to ArgumentBuilder (gets more broad)

--- comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandFragment.kt
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandFragment.kt
@@ -1,11 +1,13 @@
 package com.github.ityeri.comshop.internal.impl
 
 import com.mojang.brigadier.Command
-import com.mojang.brigadier.builder.RequiredArgumentBuilder
+import com.mojang.brigadier.builder.ArgumentBuilder
 import io.papermc.paper.command.brigadier.CommandSourceStack
 
 
 sealed class CommandFragment {
-    class ArgumentFragment<T>(val builder: RequiredArgumentBuilder<CommandSourceStack, T>) : CommandFragment()
+    class ArgumentFragment<
+            T : ArgumentBuilder<CommandSourceStack, T>
+            >(val builder: ArgumentBuilder<CommandSourceStack, T>) : CommandFragment()
     class ExecutionFragment(val command: Command<CommandSourceStack>) : CommandFragment()
 }
