77e74f52b752b1d93e1f031fafaff41f66864335
ityeri <ityeri9@gmail.com>
2026-05-31 14:14:00 +0900

    rm: Remove all unnecessary entry classes except AbstractCommandRegistrar

--- comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/entry/AbstractCommandRegistrar.kt
+++ comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/entry/AbstractCommandRegistrar.kt
@@ -1,10 +1,10 @@
 package com.github.ityeri.comshop.internal.entry
 
 import com.github.ityeri.comshop.internal.node.Node
-import com.github.ityeri.comshop.internal.node.type.NodeType
+import com.github.ityeri.comshop.internal.node.type.CommandNodeType
 import org.bukkit.plugin.java.JavaPlugin
 
-interface AbstractCommandRegistrar<S> {
+interface AbstractCommandRegistrar {
     fun init(plugin: JavaPlugin)
-    fun register(node: Node<NodeType<S>>)
+    fun register(node: Node<CommandNodeType>)
 }
