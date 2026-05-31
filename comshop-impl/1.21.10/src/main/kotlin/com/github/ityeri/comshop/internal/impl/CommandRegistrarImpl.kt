6d4c966c1b2bf790431a89b661c7bdeaa8a1b887
ityeri <ityeri9@gmail.com>
2026-05-31 14:27:34 +0900

    edit: Changed CommandRegisterImpl class's structure fit to new AbstractCommandRegistrar inerface

--- comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandRegistrarImpl.kt
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandRegistrarImpl.kt
@@ -2,25 +2,22 @@ package com.github.ityeri.comshop.internal.impl
 
 import com.github.ityeri.comshop.internal.entry.AbstractCommandRegistrar
 import com.github.ityeri.comshop.internal.node.Node
-import com.github.ityeri.comshop.internal.node.type.NodeType
-import com.mojang.brigadier.tree.LiteralCommandNode
+import com.github.ityeri.comshop.internal.node.type.CommandNodeType
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.plugin.java.JavaPlugin
 
-class CommandRegistrarImpl : AbstractCommandRegistrar<CommandSourceStack> {
-    val nodes: MutableList<Node<NodeType<CommandSourceStack>>> = mutableListOf()
-
+class CommandRegistrarImpl : AbstractCommandRegistrar {
+    val nodes: MutableList<Node<CommandNodeType>> = mutableListOf()
     override fun init(plugin: JavaPlugin) {
         plugin.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
             for (node in nodes) {
-                val boundary = createBoundary(node)
-                commands.registrar().register(boundary.entries.first().build() as LiteralCommandNode)
+//                val boundary = createBoundary(node)
             }
         }
     }
 
-    override fun register(node: Node<NodeType<CommandSourceStack>>) {
+    override fun register(node: Node<CommandNodeType>) {
         nodes.add(node)
     }
 }
