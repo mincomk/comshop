b23d455cebd3753101f7b11a5137db0ffd7ae43a
ityeri <ityeri9@gmail.com>
2026-05-31 14:12:15 +0900

    add: ComshopCustomArgumentType which is implements ComshopArgumentType

---
+++ comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/argument/ComshopCustomArgumentType.kt
@@ -0,0 +1,10 @@
+package com.github.ityeri.comshop.internal.argument
+
+import com.github.ityeri.comshop.internal.CommandWritingContext
+import com.github.ityeri.comshop.internal.SuggestionElement
+import io.papermc.paper.command.brigadier.CommandSourceStack
+
+abstract class ComshopCustomArgumentType<T, N>(val nativeArgumentType: NativeArgumentType<N>) : ComshopArgumentType<T> {
+    abstract fun parse(nativeValue: N, source: CommandSourceStack): T
+    abstract fun suggest(writingContext: CommandWritingContext, source: CommandSourceStack): Iterable<SuggestionElement>
+}
