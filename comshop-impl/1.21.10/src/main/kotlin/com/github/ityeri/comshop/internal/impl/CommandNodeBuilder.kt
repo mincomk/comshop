e92d34c24ded52ab4f762819ec5cd6133d1ff27b
ityeri <ityeri9@gmail.com>
2026-06-01 23:40:29 +0900

    add: Added CommandNodeBuilder for stable children node managing and building instead of ArgumentBuilder

---
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandNodeBuilder.kt
@@ -0,0 +1,59 @@
+package com.github.ityeri.comshop.internal.impl
+
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.arguments.ArgumentType
+import com.mojang.brigadier.suggestion.SuggestionProvider
+import com.mojang.brigadier.tree.ArgumentCommandNode
+import com.mojang.brigadier.tree.CommandNode
+import com.mojang.brigadier.tree.LiteralCommandNode
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import java.util.function.Predicate
+
+
+sealed class CommandNodeBuilder {
+    val children: MutableList<CommandNodeBuilder> = mutableListOf()
+    val command: Command<CommandSourceStack>? = null
+    val requiresChecker: Predicate<CommandSourceStack>? = null
+
+    abstract fun build(): CommandNode<CommandSourceStack>
+
+    class LiteralNodeBuilder(val literal: String) : CommandNodeBuilder() {
+        override fun build(): CommandNode<CommandSourceStack> {
+            val node = LiteralCommandNode(
+                literal,
+                command,
+                requiresChecker,
+                null,
+                null,
+                false
+            )
+
+            children.forEach { node.addChild(it.build()) }
+
+            return node
+        }
+    }
+
+    class ArgumentNodeBuilder<T>(
+        val name: String,
+        val argumentType: ArgumentType<T>,
+        val suggestionProvider: SuggestionProvider<CommandSourceStack>? = null
+    ) : CommandNodeBuilder() {
+        override fun build(): CommandNode<CommandSourceStack> {
+            val node = ArgumentCommandNode(
+                name,
+                argumentType,
+                command,
+                requiresChecker,
+                null,
+                null,
+                false,
+                suggestionProvider
+            )
+
+            children.forEach { node.addChild(it.build()) }
+
+            return node
+        }
+    }
+}
