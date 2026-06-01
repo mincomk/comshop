31921c62cc3baac4fae400a1321a3f733bedb444
ityeri <ityeri9@gmail.com>
2026-06-02 00:50:27 +0900

    refactor: SuggestionElement.component -> tooltipMessage

--- comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/argument/SuggestionElement.kt
+++ comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/argument/SuggestionElement.kt
@@ -2,4 +2,4 @@ package com.github.ityeri.comshop.internal.argument
 
 import net.kyori.adventure.text.Component
 
-data class SuggestionElement(val text: String, val component: Component? = null)
+data class SuggestionElement(val text: String, val tooltipMessage: Component? = null)
