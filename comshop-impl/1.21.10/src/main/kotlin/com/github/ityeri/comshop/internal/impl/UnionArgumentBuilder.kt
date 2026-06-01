b2750c44062b7f611badbb0a91d4ef9365528902
ityeri <ityeri9@gmail.com>
2026-06-02 00:21:41 +0900

    rm: Unused UnionArgumentBuilder

--- comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/UnionArgumentBuilder.kt
+++
@@ -1,7 +0,0 @@
-package com.github.ityeri.comshop.internal.impl
-
-import com.mojang.brigadier.builder.ArgumentBuilder
-import io.papermc.paper.command.brigadier.CommandSourceStack
-
-
-typealias UnionArgumentBuilder = ArgumentBuilder<CommandSourceStack, *>
