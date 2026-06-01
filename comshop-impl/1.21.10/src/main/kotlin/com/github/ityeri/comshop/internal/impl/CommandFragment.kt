ff19ed3050685c588fe15cb9f187f0fb3e284555
ityeri <ityeri9@gmail.com>
2026-06-01 23:48:14 +0900

    edit: Replace ArgumentFragment to NodeBuilderFragment which is includes CommandNodeBuilder instead of ArgumentBuilder

--- comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandFragment.kt
+++ comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/internal/impl/CommandFragment.kt
@@ -1,13 +1,10 @@
 package com.github.ityeri.comshop.internal.impl
 
 import com.mojang.brigadier.Command
-import com.mojang.brigadier.builder.ArgumentBuilder
 import io.papermc.paper.command.brigadier.CommandSourceStack
 
 
 sealed class CommandFragment {
-    class ArgumentFragment<
-            T : ArgumentBuilder<CommandSourceStack, T>
-            >(val builder: ArgumentBuilder<CommandSourceStack, T>) : CommandFragment()
+    class NodeBuilderFragment(val builder: CommandNodeBuilder) : CommandFragment()
     class ExecutionFragment(val command: Command<CommandSourceStack>) : CommandFragment()
 }
