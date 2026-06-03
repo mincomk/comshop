f88b4c46e0d65308a7a7e416b001c2931de1f206
ityeri <ityeri9@gmail.com>
2026-06-03 19:01:17 +0900

    add: CommandExecutionContext class at comshop-api

---
+++ comshop-api/src/main/kotlin/com/github/ityeri/comshop/context/CommandExecutionContext.kt
@@ -0,0 +1,19 @@
+package com.github.ityeri.comshop.context
+
+import com.github.ityeri.comshop.dsl.ComshopDsl
+import com.github.ityeri.comshop.internal.ComshopContext
+import kotlin.reflect.KClass
+
+
+@ComshopDsl
+class CommandExecutionContext(val context: ComshopContext) : SourceContext(context.source) {
+    infix fun <T : Any> String.to(clazz: KClass<T>): T =
+        context.getArgument(this, clazz.java)
+
+    infix fun <T : Any> String.toOrNull(clazz: KClass<T>): T? =
+        try {
+            context.getArgument(this, clazz.java)
+        } catch (_: IllegalArgumentException) {
+            null
+        }
+}
