7f6f8484f652af8d25de0eb6e779f623cf98887f
ityeri <ityeri9@gmail.com>
2026-06-03 18:59:36 +0900

    add: Add vararg constructor at Node class for convenience

--- comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/node/Node.kt
+++ comshop-interface/src/main/kotlin/com/github/ityeri/comshop/internal/node/Node.kt
@@ -2,6 +2,10 @@ package com.github.ityeri.comshop.internal.node
 
 sealed class Node<out T> {
     class SingleNode<T>(val value: T) : Node<T>()
-    class UnionNode<T>(val nodes: Iterable<Node<T>>) : Node<T>()
-    class ChainNode<T>(val nodes: Iterable<Node<T>>) : Node<T>()
+    class UnionNode<T>(val nodes: Iterable<Node<T>>) : Node<T>() {
+        constructor(vararg nodes: Node<T>): this(nodes.toList())
+    }
+    class ChainNode<T>(val nodes: Iterable<Node<T>>) : Node<T>() {
+        constructor(vararg nodes: Node<T>): this(nodes.toList())
+    }
 }
