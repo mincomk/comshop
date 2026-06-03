d973f954d62def8bc72274a5a9f9ad59ea6c9801
ityeri <ityeri9@gmail.com>
2026-06-03 18:52:44 +0900

    fix: CommandRegistrar changed to load registrar implementation from ComshopLoader

--- comshop-api/src/main/kotlin/com/github/ityeri/comshop/CommandRegistrar.kt
+++ comshop-api/src/main/kotlin/com/github/ityeri/comshop/CommandRegistrar.kt
@@ -1,6 +1,6 @@
 package com.github.ityeri.comshop
 
 import com.github.ityeri.comshop.internal.entry.AbstractCommandRegistrar
-import io.papermc.paper.command.brigadier.CommandSourceStack
 
-object CommandRegistrar : AbstractCommandRegistrar<CommandSourceStack> by ComshopBootstrapper.loader!!.loadRegistrar()
+
+object CommandRegistrar : AbstractCommandRegistrar by ComshopLoader.loadRegistrarImpl()
