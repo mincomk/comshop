f33207c2b5528c1251070277374be5149f9540ae
ityeri <ityeri9@gmail.com>
2026-06-03 19:01:02 +0900

    add: SourceContext class at comshop-api

---
+++ comshop-api/src/main/kotlin/com/github/ityeri/comshop/context/SourceContext.kt
@@ -0,0 +1,15 @@
+package com.github.ityeri.comshop.context
+
+import com.github.ityeri.comshop.dsl.ComshopDsl
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Entity
+
+
+@ComshopDsl
+open class SourceContext(val source: CommandSourceStack) {
+    val sender: CommandSender
+        get() = source.sender
+    val entity: Entity?
+        get() = source.executor
+}
